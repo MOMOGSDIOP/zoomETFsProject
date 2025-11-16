fcee907c4fcc366e35df0815d2ca1d7c1021428e
MOMOGSDIOP <mamanpro29@gmail.com>
2025-11-16 12:06:45 +0100

    Update session.py

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
-from app.core.settings import settings  # pour récupérer settings.database_url
+from backend.app.core.settings import settings  # pour récupérer settings.database_url
 from typing import AsyncGenerator
 DATABASE_URL = settings.database_url_async  # Ex: "postgresql+asyncpg://user:password@localhost/dbname"
 
