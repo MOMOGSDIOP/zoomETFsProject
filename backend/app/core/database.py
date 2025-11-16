040a32ef08d36e8a6a49d448cf943e2de6765740
MOMOGSDIOP <mamanpro29@gmail.com>
2025-11-16 13:14:47 +0100

    Update database.py

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
-from app.core.settings import settings
+from backend.app.core.settings import settings
 
 # On transforme l'URL postgres classique en asyncpg
 DATABASE_URL = settings.database_url.replace("postgresql://", "postgresql+asyncpg://")
